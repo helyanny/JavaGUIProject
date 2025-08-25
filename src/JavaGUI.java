6095558c43fee9a1801cd49b03ce5d5332eb72fb
Helyanny Perozo <helyannyperozo@mac.home>
2025-08-25 09:15:11 -0400

    First attempt at GUI

---
+++ src/JavaGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class JavaGUI {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+    }
+}
