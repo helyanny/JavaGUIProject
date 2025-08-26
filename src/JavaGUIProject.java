a0842b3e24b84ade2a25dd430a9ade791245bc1c
Helyanny Perozo <helyannyperozo@mac.home>
2025-08-26 18:48:51 -0400

    Adding text field

--- src/JavaGUIProject.java
+++ src/JavaGUIProject.java
@@ -1,20 +1,28 @@
 import javax.swing.*;
 
-// First attempt at GUI
+// My first attempt at a GUI
 public class JavaGUIProject {
 
-
-
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(500, 150);
+
+        // Making the frame
+        JFrame frame = new JFrame("hely's wonderful GUI");
+        frame.setSize(500, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
-        JButton button = new JButton("Click here!");
-        button.setBounds(100, 100, 50, 50);
+        // Playing around with elements
+        JTextField textField = new JTextField();
+        textField.setBounds(100, 100, 200, 50);
 
+        JButton button = new JButton("Click here!");
+        button.setBounds(180, 20, 50, 25);
+
+
+        // Adding elements to the frame
+        frame.add(textField);
         frame.add(button);
+
 
         frame.setVisible(true);
     }
