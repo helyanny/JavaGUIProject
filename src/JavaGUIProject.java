0a7c3642341b58016dac948753fecfafed1cb3a9
Helyanny Perozo <helyannyperozo@mac.home>
2025-08-26 18:29:00 -0400

    Adding a button

--- src/JavaGUIProject.java
+++ src/JavaGUIProject.java
@@ -3,13 +3,18 @@ import javax.swing.*;
 // First attempt at GUI
 public class JavaGUIProject {
 
+
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(500, 500);
+        frame.setSize(500, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
         JButton button = new JButton("Click here!");
+        button.setBounds(100, 100, 50, 50);
+
+        frame.add(button);
 
         frame.setVisible(true);
     }
